53334058af043cc9d48031b4e8e8923614364ccb
Nekitos <nikita.host@gmail.com>
2014-11-23 22:17:21 +0400

    First commit

---
+++ sqlscripts/group_table.sql
@@ -0,0 +1,5 @@
+CREATE TABLE group_table (
+	id BIGINT PRIMARY KEY,
+	g_number INT,
+	faculty VARCHAR(60)
+);
---
+++ sqlscripts/students_table.sql
@@ -0,0 +1,8 @@
+CREATE TABLE students_table (
+	id BIGINT PRIMARY KEY,
+	first_name VARCHAR(30), 
+	last_name VARCHAR(30), 
+	third_name VARCHAR(30), 
+	birthday DATE,
+	group_id INT
+);
